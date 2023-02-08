d623b741c856424ecf0ce2e1eabcf427447fe8e3
lenorium <lenorium>
2023-02-08 15:13:57 +0300

    задачи разделены по типам запросов

---
+++ easy/joins.sql
@@ -0,0 +1,38 @@
+'''Popularity of Hack
+
+https://platform.stratascratch.com/coding/10061-popularity-of-hack?code_type=1
+
+Meta/Facebook has developed a new programing language called Hack.
+To measure the popularity of Hack they ran a survey with their employees.
+The survey included data on previous programing familiarity as well as the number of years of experience,
+age, gender and most importantly satisfaction with Hack. Due to an error location data was not collected,
+but your supervisor demands a report showing average popularity of Hack by office location.
+Luckily the user IDs of employees completing the surveys were stored.
+Based on the above, find the average popularity of the Hack per office location.
+Output the location along with the average popularity.
+
+Tables: facebook_employees, facebook_hack_survey
+
+facebook_employees
+
+id:        int
+location:  varchar
+age:       int
+gender:    varchar
+is_senior: bool
+
+facebook_hack_survey
+
+employee_id: int
+age:         int
+gender:      varchar
+popularity:  int'''
+
+select e.location,
+       avg(s.popularity)
+  from facebook_hack_survey as s
+ inner join facebook_employees as e
+    on s.employee_id = e.id
+ group by location;
+
+-- --------------------------------------------------------------------------------------------------------------------
